70efe38f1efd02e87c1b84019b937878fa3bac42
Tilo Prütz <tilo@infopark.de>
2017-10-06 08:33:19 +0200

    fixed parallel deployment
    - already deployed parts does not fail the whole thing anymore
    - already deployed parts are log notifications instead of errors
    - only one DRI announcement (if at least one part was deployed)
    - better log messages for deployments (including function name)

--- index.js
+++ index.js
@@ -45,13 +45,24 @@ function deployDefinitions() {
   return _definitions;
 }
 
+var _commit;
+function commitPromise() {
+  if (!_commit) {
+    _commit = $("git rev-parse @").promise();
+  }
+  return _commit;
+}
+
+var _basedir;
+function basedirPromise() {
+  if (!_basedir) {
+    _basedir = $("git rev-parse --show-toplevel").promise();
+  }
+  return _basedir;
+}
+
 function createPackage(name, packageJson) {
-  return Promise.all([
-    $("git rev-parse @").promise(),
-    $("git rev-parse --show-toplevel").promise(),
-  ]).then((commit_and_basedir) => {
-    var commit = commit_and_basedir[0];
-    var basedir = commit_and_basedir[1];
+  return Promise.all([commitPromise(), basedirPromise()]).then(([commit, basedir]) => {
     var packagedir = `${basedir}/pkg`;
     var packagePath = `${packagedir}/${name}_${commit}.zip`;
 
@@ -101,21 +112,23 @@ task('package', {async: true}, function () {
   chain.then(() => complete(packages)).catch(e => promiseFail(e));
 });
 
+class Cancel extends Error {
+}
+
 function performDeployment(FunctionName, force) {
   const Aws = require('aws-sdk');
-  const sleep = require('sleep-promise');
-  console.log(`Deploying to AWS profile ${Aws.config.credentials.profile}.`);
+  console.log(`Deploying to ${FunctionName} in AWS profile ${Aws.config.credentials.profile}.`);
 
   var package = jake.Task['package'].value[FunctionName];
   var lambda = new Aws.Lambda({region: 'eu-west-1'});
-  lambda.listAliases({FunctionName}).promise().then((result) => {
+  return lambda.listAliases({FunctionName}).promise().then((result) => {
     return result.Aliases.find((alias) => { return alias.Name == 'active'; });
   }).then((activeAlias) => {
     return lambda.listVersionsByFunction({FunctionName}).promise().then((result) => {
       var activeVersion = result.Versions.find(
           (version) => { return version.Version == activeAlias.FunctionVersion; });
       if (activeVersion.Description == package.commit && !force) {
-        throw `Commit ${package.commit} is already deployed.`;
+        throw new Cancel(`Commit ${package.commit} is already deployed at ${FunctionName}.`);
       }
     });
   }).then(() => {
@@ -132,54 +145,70 @@ function performDeployment(FunctionName, force) {
       FunctionVersion: result.Version,
     }).promise();
   }).then(() => {
-    console.log("Deployment done.");
+    console.log(`Deployed ${FunctionName}.`);
+    return true;
+  }).catch(e => {
+    if (e instanceof Cancel) {
+      console.log(e.message);
+      return false;
+    } else {
+      throw e;
+    }
+  });
+}
 
-    if (!process.env.EDITOR) { throw "EDITOR is not set."; }
-    if (!process.env.DRI_SLACK_WEBHOOK_URI) { throw "DRI_SLACK_WEBHOOK_URI is not set."; }
+function sendDRINotification() {
+  if (!process.env.EDITOR) { throw "EDITOR is not set."; }
+  if (!process.env.DRI_SLACK_WEBHOOK_URI) { throw "DRI_SLACK_WEBHOOK_URI is not set."; }
 
-    console.log(`Now starting ${process.env.EDITOR} to edit the DRI announcement message…`);
-    return Promise.all([
-      $("git remote get-url origin").promise(),
-      sleep(2000).then(() => {
-        return new Promise((resolve, reject) => {
-          var tmpFile = Tempy.file();
-          jake.exec([`${process.env.EDITOR} ${tmpFile}`], {interactive: true}, () => {
-            resolve(fs.readFileSync(tmpFile));
-          });
+  const sleep = require('sleep-promise');
+  const Path = require('path');
+
+  console.log(`Now starting ${process.env.EDITOR} to edit the DRI announcement message…`);
+  return Promise.all([
+    basedirPromise().then(basedir => Path.basename(basedir)),
+    commitPromise(),
+    $("git remote get-url origin").promise(),
+    sleep(2000).then(() => {
+      return new Promise((resolve, reject) => {
+        var tmpFile = Tempy.file();
+        jake.exec([`${process.env.EDITOR} ${tmpFile}`], {interactive: true}, () => {
+          resolve(fs.readFileSync(tmpFile));
         });
-      }),
-    ]).then((repoUrl_driMessage) => {
-      var repoUrl = repoUrl_driMessage[0];
-      var driMessage = repoUrl_driMessage[1];
-
-      slackr = require('slackr');
-      slackr.conf.uri = process.env.DRI_SLACK_WEBHOOK_URI;
-      if (repoUrl.startsWith("git@github.com:")) {
-        repoUrl = `https://github.com/${repoUrl.slice(15)}`;
-      }
-      var commitUrl = repoUrl;
-      if (repoUrl.startsWith("https://github.com")) {
-        commitUrl = `${repoUrl}/commit/${package.commit}`;
-      }
-
-      var announcement = `:aws: :lambda: (${FunctionName}) ` +
-          `<@${process.env.SLACK_USER || process.env.USER}> deployed ` +
-          `<${commitUrl}|#${package.commit.substr(0, 8)}>: ${driMessage}`;
-      return slackr.string(announcement).then(() => {
-        console.log(`Sent DRI announcement: ${announcement}`);
       });
-    }).catch((error) => {
-      console.log(`Could not notify DRI channel: ${error}`);
-      console.log("Don't forget to post an announcement there.");
+    }),
+  ]).then(([serviceName, commit, repoUrl, driMessage]) => {
+    slackr = require('slackr');
+    slackr.conf.uri = process.env.DRI_SLACK_WEBHOOK_URI;
+    if (repoUrl.startsWith("git@github.com:")) {
+      repoUrl = `https://github.com/${repoUrl.slice(15)}`;
+    }
+    var commitUrl = repoUrl;
+    if (repoUrl.startsWith("https://github.com")) {
+      commitUrl = `${repoUrl}/commit/${commit}`;
+    }
+
+    var announcement = `:aws: :lambda: (${serviceName}) ` +
+        `<@${process.env.SLACK_USER || process.env.USER}> deployed ` +
+        `<${commitUrl}|#${commit.substr(0, 8)}>: ${driMessage}`;
+    return slackr.string(announcement).then(() => {
+      console.log(`Sent DRI announcement: ${announcement}`);
     });
-  }).then(() => {
-    complete();
-  }).catch((e) => { promiseFail(e); });
+  }).catch((error) => {
+    console.log(`Could not notify DRI channel: ${error}`);
+    console.log("Don't forget to post an announcement there.");
+  });
 }
 
 desc("Deploys the package on AWS.");
 task('deploy', ['package'], {async: true}, function(force) {
+  var deployments = [];
   for (let definition of deployDefinitions()) {
-    performDeployment(definition.functionName, force);
+    deployments.push(performDeployment(definition.functionName, force));
   }
+  Promise.all(deployments).then(deployed => {
+    if (deployed.some(d => d)) {
+      return sendDRINotification();
+    }
+  }).then(complete).catch(promiseFail);
 });
